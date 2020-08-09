942d1543afdb3209d3ce9d4fa3a27db80b6abf53
Oleh-Viktor <syrytsiaviktor@gmail.com>
2020-08-10 00:15:37 +0300

    add semantic link

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const App = () => {
         return (
                 <div>
-                        Hello world
+                        Hello world!
                 </div>
         )
 }
 
 export default App
+
