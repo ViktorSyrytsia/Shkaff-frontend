a8074f1134aa438b8d33c991c23027a7afb3e6f1
Dmytro Didukh <jeriko.outback@gmail.com>
2020-08-10 19:05:35 +0300

    loading page & local fonts

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as App } from './app'
 export { default as Categories } from './categories'
 export { default as Footer } from './footer';
+export { default as LoadingPage } from './LoadingPage';
+export { default as Spinner } from './Spinner';
