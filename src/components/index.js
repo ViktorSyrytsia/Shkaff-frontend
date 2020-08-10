b3a949d596ecf2400b1d2493a04d24697173891d
Oleh-Viktor <syrytsiaviktor@gmail.com>
2020-08-10 19:12:19 +0300

    conflict resolve

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,7 @@
 export { default as App } from './app'
 export { default as Categories } from './categories'
 export { default as Footer } from './footer';
-<<<<<<< HEAD
 export { default as ProductCard } from './productCard';
 export { default as SubcategoryFilter } from './subcategoryFilter';
-=======
+export { default as Spinner } from './Spinner/';
 export { default as LoadingPage } from './LoadingPage';
-export { default as Spinner } from './Spinner';
->>>>>>> dd3e2eba11b87834c4740ae38bb351525508937a
